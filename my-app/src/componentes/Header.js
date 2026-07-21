7537afb195d7b9c718964a8f19e404625cb12044
helenalacerda06 <helenacardoso062009@gmail.com>
2026-07-20 21:42:27 -0300

    :)

---
+++ my-app/src/componentes/Header.js
@@ -0,0 +1,12 @@
+import styles from './Header.css';
+
+function Header() {
+    return (
+        <Header className={styles.Header}>
+            <h1>Welcome to My App</h1>
+        </Header>
+    );
+}
+
+
+export default Header;
