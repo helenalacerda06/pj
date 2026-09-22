449ec20e2f1426f51ad7813e82f9a2e86e702f39
Helena <helenacardoso062009@gmail.com>
2026-09-22 17:28:38 -0300

    :)

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,35 @@
 const express = require("express")
+const sqlite3 = require("sqlite3").verbose()
 
 const server = express()
 
-server.get('/usuario', (request, response) => {
-    return response.send("Usuario criado com sucesso")
+server.use(express.json())
+
+const db = new sqlite3.Database("../teste_helena.db", (erro) => {
+    if (erro) {
+        console.log("Erro ao conectar com o banco:", erro.messagem)
+    } else {
+        console.log("Banco conectado com sucesso!")
+    }
 })
 
-server.listen(3001)
+server.post('/usuario', (request, response) => {
+
+    const { nome, email, senha } = request.body
+
+    const sql = `
+        INSERT INTO usuarios (nome, email, senha)
+    VALUES(?, ?, ?)
+        `
+
+    db.run(sql, [nome, email, senha], function(erro){
+        if (erro){
+            return response.status(500).send("Erro ao cria usuário")
+        }
+        return response.send("Usuario criado com sucesso")
+    })
+})
+
+server.listen(3001, () => {
+    console.log("Servidor rodando em http://localhost:3001")
+})
